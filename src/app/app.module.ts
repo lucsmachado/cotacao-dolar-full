1390a3e78f46d178a5b023ed768e88d959c69fb5
Lucas Machado <lcmachado.cs@gmail.com>
2024-02-07 13:11:10 -0300

    feat: replace checkbox with material slide toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatSlideToggleModule,
   ],
   providers: [DatePipe, CurrencyPipe],
   bootstrap: [AppComponent],
